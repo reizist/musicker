ca04657b2192a24721baf838f903784f3ccc0e49
reizist <reizist@gmail.com>
2014-05-11 16:15:38 +0900

    railsAdmin導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
