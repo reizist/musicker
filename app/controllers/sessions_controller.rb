b2f932fb4151da2cc58bf1a8ec1392d30a0d9184
reizist <reizist@gmail.com>
2014-06-01 18:33:24 +0900

    fix session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,23 @@
 class SessionsController < ApplicationController
-
-  API_KEY = 'f2021dbda43cd3fcf835dfd607c03124'
-  API_SECRET = 'd725ed6474f61d59b5797f73489e4709'
   def new
-    lastfm = Lastfm.new(API_KEY, API_SECRET)
-    token = lastfm.auth.get_token
   end
 
   def create
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Signed In!"
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
