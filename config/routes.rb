48a19605323bdcc10dba90ae84735138a0049df4
reizist <reizist@gmail.com>
2014-06-01 18:35:28 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/signin',  to: 'sessions#new',         via: 'get'
+  match "/auth/:provider/callback", to: "sessions#create", via: 'get'
+  match "/signout", to: "sessions#destroy", via: 'get'
 
   resources :songs
 
