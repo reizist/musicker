78c7a6f410fb4b2cfe50e09b2635ef9792b049e1
reizist <reizist@gmail.com>
2014-06-04 01:07:10 +0900

    add methods get_user/get_recent_songs to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,16 @@ class User < ActiveRecord::Base
     end
   end
 
+  BASE_URL = 'http://ws.audioscrobbler.com/2.0/?'
+
+  def get_user_info
+    url = "http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=#{self.uid}&api_key=f2021dbda43cd3fcf835dfd607c03124&format=json"
+    JsonParser::parse(url)
+  end
+
+  def get_recent_songs
+    url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=#{self.uid}&api_key=f2021dbda43cd3fcf835dfd607c03124&format=json"
+    JsonParser::parse(url)
+  end
 
 end
